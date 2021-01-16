86d294b666a5b59ab7b8786dfd2779de4664ffe3
syd333 <sydmiranda@icloud.com>
2021-01-16 16:20:06 -0500

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,10 @@
+#learn spec/01_book_spec.rb
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -9,5 +13,11 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+#create the writer for genre and add the logic for the class constant
+def genre=(genre)
+  @genre = genre 
+  GENRES << genre
+end
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,19 @@
+# spec/02_shoe_spec.rb
+
+
 class Shoe
+  BRANDS = []
+
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+    BRANDS << brand 
   end
+end
+
 
   def cobble
     self.condition = "new"
